d31e7efd60fc2705308d986a05550a15d4260fcc
Samson Sim <samsonsim2@gmail.com>
2023-03-06 01:14:52 +0800

    update report page

--- src/pages/Report Page/Report.js
+++ src/pages/Report Page/Report.js
@@ -5,8 +5,22 @@ import { useAppContext } from '../../context/appContext'
 const Report = () => {
      
   return (
-     
-        <Form/>
+     <div className="flex justify-center">
+
+        {/* <FormBox/> */}
+       
+      <div className="flex flex-col flex-wrap  w-5/6  ">
+        <div   className="flex justify-center gap-5 m-10" >
+          <button   className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Missing</button>
+          <button   className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Spotted</button>
+        </div>
+        <Form/> 
+      </div>
+      
+
+
+     </div>
+        
   )
 }
 
