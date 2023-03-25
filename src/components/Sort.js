0b02f0f1adcfdb93e84a5184290885ff90ea85f8
Samson Sim <samsonsim2@gmail.com>
2023-03-25 11:27:48 +0800

    mobile responsiveness

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -68,7 +68,7 @@ const Sort = () => {
   };
 
   return (
-    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       {/* Reset Filter Button */}
       <div className="grid grid-cols-2">
         <label className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
--- src/pages/Feed Page/Feed.js
+++ src/pages/Feed Page/Feed.js
@@ -15,12 +15,34 @@ import Card from "../../components/Card";
 import ListItem from "../../components/ListItem";
 //DB stuff
 import { useNavigate } from "react-router-dom";
+import PurpleButton from "../../components/PurpleButton";
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute'  ,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const DB_REPORT_KEY = "report";
 const reportRef = ref(database, DB_REPORT_KEY);
 const Feed = () => {
   const { values, setValues, setReport } = useAppContext();
   const navigate = useNavigate();
   const [view, setView] = useState("list");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   useEffect(() => {
     setValues((report) => {
       return {
@@ -44,14 +66,46 @@ const Feed = () => {
   // console.log(values.allReports)
   return (
     <>
-      <div className="grid grid-cols-5 min-h-screen bg-slate-200">
+      <div className="grid xs:grid-cols-1 md:grid-cols-5 min-h-screen bg-slate-200">
         {/*- Filter Nav*/}
-        <div className="  col-span-1   p-5 border-black">
+        <div className="  hidden md:block col-span-1    p-5 border-black">
           <Sort />
         </div>
         {/*- REPORT GRID*/}
-        <div className=" col-span-4 p-5">
-          <div class=" bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
+
+
+         {/*- Filter button for mobile*/}
+
+         <div className="xs:block md:hidden flex justify-center p-5 ">
+
+         <button
+        type="submit"
+        className="w-3/6 inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+        onClick={handleOpen}
+     >
+                    Filter
+     </button>
+      
+      
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="flex justify-center "
+        
+      >
+        <div className="w-5/6 self-center">   <Sort/></div>
+    
+      </Modal>
+    </div>
+
+        
+
+
+      
+        <div className=" xs:col-span-5 md:col-span-4  p-5  ">
+          <div class=" bg-gray-200 text-sm text-gray-500 leading-none   rounded-full inline-flex">
             <button
               onClick={() => setView("grid")}
               className={
@@ -111,19 +165,20 @@ const Feed = () => {
               <span>List</span>
             </button>
           </div>
-          <text className="ml-5">
+         
+          <text className="ml-5 text-xs">
             {values.filteredReports.length} reports found{" "}
           </text>
-          <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+          <hr class="h-px my-5 bg-gray-200   dark:bg-gray-700"></hr>
           {values.filteredReports.length === 0 ? (
-            <div className="mt-5">No Results...</div>
+            <div className="mt-5 bg-indigo-200 border-10">No Results...</div>
           ) : (
-            <div>
+            <div className=" ">
               <div
                 className={
                   view === "list"
-                    ? "grid grid-cols-1 gap-5"
-                    : "grid grid-cols-4 gap-5"
+                    ? "grid grid-cols-1 gap-5 "
+                    : "grid xs:grid-cols-1 md:grid-cols-4 gap-5"
                 }
               >
                 {values.filteredReports &&
